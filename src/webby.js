f34c4217026f7b1f0d90f89e6763f92b8bd9ee97
aman <darkknight29@live.hk>
2018-10-08 01:24:38 -0400

    Not quite working middleware

--- src/webby.js
+++ src/webby.js
@@ -1,4 +1,6 @@
 // webby.js
+const net = require('net');
+const path = require('path');
 
 const webFrameWork = {
 
@@ -39,7 +41,150 @@ const webFrameWork = {
     }
   },
 
-  
+  //Class that parses out method and path from http request
+  Request: class{
+    constructor(httpRequest){
+      const [method, path, ...elseInfo] = httpRequest.split(" ");
+      this.method = method;
+      this.path = path;
+    }
+  },
+
+  /*Class that represents the web application
+    1. Accepts and parse http request
+    2. Hold http method and path combos
+    3. Optionally call middleware if it exists
+    4. Or determine what to do with incoming request path
+    5. Send back valid http response
+  */
+  App: class{
+    //Creates and initializes application
+    constructor (){
+      this.server = net.createServer(sock => this.handleConnection(sock));
+      this.routes = {};
+      this.middleware = null;
+    };
+
+    //Function takes path and normalizes casing and trailing slash
+    //Also removes query string if present
+    normalizePath(inPath){
+      inPath = inPath.toLowerCase();
+      inPath = inPath.split("?")[0].split("#")[0].split("/");
+      return "/"+inPath[1];
+    };
+
+    //Function that takes http method and path
+    //Return string rep. of the parameters for ROUTES property
+    createRouteKey(method, path){
+      return `${method.toUpperCase()} ${this.normalizePath(path)}`;
+    };
+
+    //Function that puts callback function of key (from path)
+    //into routes property
+    get (path, cb){
+      const key = this.createRouteKey("get", path);
+      this.routes[key] = cb;
+    };
+
+    //Function that sets middleware to app
+    use (cb) {
+      this.middleware = cb;
+    };
+
+    //Function that calls listen function from server with set parameters
+    listen (port, host) {
+      this.server.listen(port, host);
+    };
+
+    //Function that handles connection
+    handleConnection(sock) {
+      const singleHandleRequest = this.handleRequest.bind(sock, null);
+      sock.on("data", binaryData => singleHandleRequest(binaryData));
+    };
+
+    //Function that handles request
+    handleRequest(sock, binaryData){
+      const s = binaryData+"";
+      const req = new webFrameWork.Request(s);
+      const res = new webFrameWork.Response(sock);
+      if (this.middleware!==null){
+        this.middleware(req, res, this.processRoutes.bind(this, req, res));
+      } else {
+        this.processRoutes(req, res);
+      }
+    };
+
+    //Function that processes routes
+    processRoutes(req, res){
+      const key = this.createRouteKey(req.method, req.path);
+      if (this.routes[key]!==null){
+        const fn = this.routes[key];
+        fn(req, res);
+      } else {
+        res.status(404);
+        //res.send(HTTP_STATUS_CODES[res.statusCode]);
+      }
+    };
+
+  },
+
+  //Response class
+  Response: class {
+    constructor(socket, statusCode, version) {
+      this.sock = socket;
+      this.headers = {};
+      this.body = "";
+      this.version = "HTTP/1.1";
+      this.statusCode = 200;
+      if (version!==undefined)
+        this.version = version;
+      if (statusCode!==undefined)
+        this.statusCode = statusCode;
+    };
+
+    //Function that sets headers objects
+    set(name, value) {
+      this.headers[name] = value;
+    };
+
+    //Function that ends connection with Server
+    end() {
+      this.sock.end("Connection has ended");
+    };
+
+    //Function that outputs status
+    statusLineToString() {
+      return `${this.version} ${this.statusCode} ${HTTP_STATUS_CODES[this.statusCode]}`+"\r\n";
+    };
+
+    //Function that outputs headers
+    headersToString() {
+      let headers = "";
+      for (let h in this.headers) {
+        if (this.headers.hasOwnProperty(h)) {
+          headers+= h+": "+this.headers[h]+"\r\n";
+        }
+      }
+      return headers;
+    };
+
+    //Function that sets body of response obj
+    send(body) {
+      this.response = body;
+      this.sock.write(this.statusLineToString());
+      this.sock.write(this.headersToString());
+      this.sock.write("\r\n");
+      this.sock.write(body);
+      this.end();
+    };
+
+    //Functio that sets status code and return new response obj
+    status(statusCode) {
+      this.statusCode = statusCode;
+      return this;
+    };
+
+  },
 
 };
 
